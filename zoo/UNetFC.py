e11102d25ff0312462cf0bba5feed232c160486c
Balaban Stefan-Alexandru <162574101+AlexandruBlbn@users.noreply.github.com>
2025-12-13 20:59:55 +0200

    no updates.

--- zoo/UNetFC.py
+++ zoo/UNetFC.py
@@ -1,5 +1,20 @@
 import torch
-import torch.nn as nn
 import torchvision
-import torchsummary as summary
+from torchinfo import summary
+import timm
+
+
+model = torchvision.models.convnext_tiny()
+print("ConvNeXt Tiny Model Summary:")
+summary(model, input_size=(1, 3, 224, 224)) 
+
+model = torchvision.models.swin_v2_t()
+print("Swin V2 Tiny Model Summary:")
+summary(model, input_size=(1, 3, 224, 224))
+print("ResNet50 Model Summary:")
+model = timm.create_model('resnet50', pretrained=False)
+summary(model, input_size=(1, 3, 224, 224))
+print("ViT Tiny Model Summary:")
+model = timm.create_model('vit_small_patch16_224', pretrained=False)
+summary(model, input_size=(1, 3, 224, 224))
 
