361fb16c17f7ceb0925ecd1b8d7085177364293a
Balaban Stefan-Alexandru <162574101+AlexandruBlbn@users.noreply.github.com>
2025-12-17 14:24:02 +0200

    .

--- engine/mim_trained.py
+++ engine/mim_trained.py
@@ -10,6 +10,21 @@ from torch.utils.data import DataLoader
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+import yaml
+import torch.optim as optim
+from utils.helpers import set_seed
+import torch
+import tqdm
+from zoo.mim import 
+
+
+
+def configLoader(config_path):
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+config = configLoader(r'D:\Collateral Coronary Vessels XAI\config\mim_config.yaml')
 
 def get_mim_dataloader(json_path, batch_size=32):
     base_dataset = ARCADEDataset(
@@ -19,9 +34,9 @@ def get_mim_dataloader(json_path, batch_size=32):
     )
 
     mask_gen = MaskGenerator(
-        input_size=256, 
-        mask_patch_size=32, 
-        mask_ratio=0.5
+        input_size=config['model']['input_size'], 
+        mask_patch_size=config['data']['mask_patch_size'], 
+        mask_ratio=config['data']['mask_ratio']
     )
 
     mim_dataset = ArcadeDatasetMIM(
@@ -31,35 +46,106 @@ def get_mim_dataloader(json_path, batch_size=32):
 
     loader = DataLoader(
         mim_dataset, 
-        batch_size=batch_size, 
+        batch_size=config['data']['batch_size'], 
         shuffle=True, 
-        num_workers=4,
+        num_workers=config['data']['num_workers'],
         pin_memory=True
     )
     
     return loader
 
-if __name__ == "__main__":
-    loader = get_mim_dataloader(r"D:\Collateral Coronary Vessels XAI\data\ARCADE\processed\dataset.json")
-    imgs, masks = next(iter(loader))
+def optimizer(lr=config['optimizer']['lr'], weight_decay=config['optimizer']['weight_decay']):
+    optimizer = optim.AdamW(
+        params=[],  
+        lr=lr,
+        weight_decay=weight_decay
+    )
+    return optimizer
 
-    print(f"img: {imgs.shape}") # [32, 1, 256, 256]
-    print(f"masca:  {masks.shape}") # [32, 256, 256]
+def setupSystem():
+    set_seed(config['system']['seed'])
+    device = torch.device(config['system']['device'] if torch.cuda.is_available() else 'cpu')
+    return device
+
     
-    fig, axes = plt.subplots(2, 4, figsize=(12, 6))
-    for i in range(8):
-        row = i // 4
-        col = i % 4
-        ax = axes[row, col]
-        
-        img = imgs[i].cpu().numpy().squeeze()  # [256, 256]
-        mask = masks[i].cpu().numpy()  # [256, 256]
-        
-        masked_img = img * mask
-        
-        ax.imshow(masked_img, cmap='gray')
-        ax.set_title(f"Sample {i+1}")
-        ax.axis('off')
+
+data_loader = get_mim_dataloader(
+    json_path=r'D:\Collateral Coronary Vessels XAI\data\ARCADE\processed\dataset.json',
+    batch_size=config['data']['batch_size']
+)
+
+
+def train(epochs = config['optimizer']['epochs']):
+    running_loss = 0.0
+    device = setupSystem()
+    optimizier = optimizer(
+        lr=config['optimizer']['lr'], 
+        weight_decay=config['optimizer']['weight_decay']
+    )
     
-    plt.tight_layout()
-    plt.show()
+    for epoch in range(epochs):
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     loader = get_mim_dataloader(r"D:\Collateral Coronary Vessels XAI\data\ARCADE\processed\dataset.json")
+#     imgs, masks = next(iter(loader))
+
+#     print(f"img: {imgs.shape}") # [32, 1, 256, 256]
+#     print(f"masca:  {masks.shape}") # [32, 256, 256]
+    
+#     fig, axes = plt.subplots(2, 4, figsize=(12, 6))
+#     for i in range(8):
+#         row = i // 4
+#         col = i % 4
+#         ax = axes[row, col]
+        
+#         img = imgs[i].cpu().numpy().squeeze()  # [256, 256]
+#         mask = masks[i].cpu().numpy()  # [256, 256]
+        
+#         masked_img = img * mask
+        
+#         ax.imshow(masked_img, cmap='gray')
+#         ax.set_title(f"Sample {i+1}")
+#         ax.axis('off')
+    
+#     plt.tight_layout()
+#     plt.show()
