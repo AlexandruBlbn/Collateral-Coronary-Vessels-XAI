ffb5388c74277f950bc5b85bab0208115970b217
Balaban Stefan-Alexandru <162574101+AlexandruBlbn@users.noreply.github.com>
2026-06-03 08:34:29 +0300

    chore: Remove unused .gitkeep files and clean up VasoJEPA module

--- vasojepa/__init__.py
+++
@@ -1,5 +0,0 @@
-from .sigreg import SIGReg, DeepSIGReg
-from .anatomical_tokens import AnatomicalTokens
-from .predictor import ConditionedPredictor
-from .model import VasoJEPA
-from .dataset import AngiogramDataset
